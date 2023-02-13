0586a384808494a2ebb935e81722979345df5100
Ryland Adams <rylandadams@yahoo.com>
2023-02-12 16:02:14 -0800

    css for shop page and fixed image syntax error

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import Product from './product';
+
+import './shop.css';
+import { PRODUCTS } from '../../products';
 
 const Shop = () => {
+	console.log(PRODUCTS);
 	return (
 		<div className='shop'>
 			<div className='shopTitle'>
 				<h1> RyGuys Tech </h1>
 			</div>
-			<div className='products'></div>
+			<div className='products'>
+				{PRODUCTS.map((product) => (
+					<Product data={product} />
+				))}
+			</div>
 		</div>
 	);
 };
